653d701633cf967f38bdfaf7f140320d429eedd3
Marina120803 <petrenkomarina567@gmail.com>
2023-03-23 13:00:44 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.textBoxXmin = new System.Windows.Forms.TextBox();
+            this.textBoxXmax = new System.Windows.Forms.TextBox();
+            this.textBoxStep = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(182, 294);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(75, 27);
+            this.buttonCalc.TabIndex = 0;
+            this.buttonCalc.Text = "Расчет";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // textBoxXmin
+            // 
+            this.textBoxXmin.Location = new System.Drawing.Point(97, 66);
+            this.textBoxXmin.Name = "textBoxXmin";
+            this.textBoxXmin.Size = new System.Drawing.Size(100, 22);
+            this.textBoxXmin.TabIndex = 1;
+            // 
+            // textBoxXmax
+            // 
+            this.textBoxXmax.Location = new System.Drawing.Point(97, 143);
+            this.textBoxXmax.Name = "textBoxXmax";
+            this.textBoxXmax.Size = new System.Drawing.Size(100, 22);
+            this.textBoxXmax.TabIndex = 2;
+            // 
+            // textBoxStep
+            // 
+            this.textBoxStep.Location = new System.Drawing.Point(97, 220);
+            this.textBoxStep.Name = "textBoxStep";
+            this.textBoxStep.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStep.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 26);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineWidth = 3;
+            this.chart1.BorderSkin.BorderColor = System.Drawing.Color.Red;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(428, 97);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "sin(x)";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "cos(x)";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(304, 188);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxStep);
+            this.Controls.Add(this.textBoxXmax);
+            this.Controls.Add(this.textBoxXmin);
+            this.Controls.Add(this.buttonCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.TextBox textBoxXmin;
+        private System.Windows.Forms.TextBox textBoxXmax;
+        private System.Windows.Forms.TextBox textBoxStep;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Lab10/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            double Xmin = -1.5;
+            double Xmax = 3.5;
+            double Step = 0.5;
+            textBoxStep.Text = Step.ToString();
+            textBoxXmin.Text = Xmin.ToString();
+            textBoxXmax.Text = Xmax.ToString();
+            // Количество точек графика
+            double p = (Xmax - Xmin) / Step;
+            int count = (int)Math.Ceiling(p) + 1;
+            // Массив значений X – общий для обоих графиков
+            double[] x = new double[count];
+            // Два массива Y – по одному для каждого графика
+            double[] y1 = new double[count];
+            double[] y2 = new double[count];
+
+            // Расчитываем точки для графиков функции
+            for (int i = 0; i < count; i++)
+            {
+                double a = 0;
+                double b = 0.8;
+                double c = 0;
+                // Вычисляем значение X
+                x[i] = Xmin + Step * i;
+                // Вычисляем значение функций в точке x
+                y1[i] = (Math.Pow(x[i], 5 / 2) - b) * (Math.Log((Math.Pow(x[i], 2) + 12.7)));
+                y2[i] = Math.Cos(x[i]);
+            }
+            // Настраиваем оси графика
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+
+            // Определяем шаг сетки
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+
+            // Добавляем вычисленные значения в графики
+            chart1.Series[0].Points.DataBindXY(x, y1);
+            chart1.Series[1].Points.DataBindXY(x, y2);
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
